c14f7b3a68dd38b2caa74388be6158f971b5d2c5
Slavcho Mitrov <slavemitrov@yahoo.com>
2018-01-20 19:18:13 +0100

    Add logic to display single letter word begining with vowel concatenated with 'way' at end

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
+var translation = function(sentance) {
+  var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+  vowels.forEach(function(vowel) {
+    if (sentance.includes(vowel)) {
+      sentance += "way";
+    }
+  });
+  return sentance;
+}
+
 $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
     var sentance = $("#sentance").val();
-
-    $(".translation").text(sentance);
+    var result = translation(sentance);
+    $(".translation").text(result);
   });
 });
