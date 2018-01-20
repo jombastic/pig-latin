16a83b2bc5b131a7208ab1dbd3d0fbb755451616
Slavcho Mitrov <slavemitrov@yahoo.com>
2018-01-20 19:19:30 +0100

    Add logic to display single letter word begining with vowel concatenated with 'way' at end, regardles of case

--- js/scripts.js
+++ js/scripts.js
@@ -13,7 +13,7 @@ $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var sentance = $("#sentance").val();
+    var sentance = $("#sentance").val().toLowerCase();
     var result = translation(sentance);
     $(".translation").text(result);
   });
