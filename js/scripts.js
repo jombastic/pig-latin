3867da5e065685e9bd73eff2fea815e32a9f638f
Slavcho Mitrov <slavemitrov@yahoo.com>
2018-01-28 17:45:59 +0100

    Add logic to handle non-alphabetical characters

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,13 @@
-var translation = function(words) {
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
-  var result = [];
-
-  words.forEach(function(word) {
-    vowels.forEach(function(vowel) {
-      if (word.indexOf(vowel) === 0) {
-        word += "way";
-      }
-    });
-    result.push(word);
-  });
-
-  return result.join(" ");
+var pigLatinTranslator = function(word) {
+  return word;
 };
 
 $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
-    var words = $("#sentance").val().toLowerCase().split(" ");
-    var result = translation(words);
-    $(".translation").text(result);
+    var word = $("#sentance").val();
+    var result = pigLatinTranslator(word);
+    $("#translation p").text(result);
   });
 });
