f363d441f9960d695c6af8026df92ce29910dbd5
Slavcho Mitrov <slavemitrov@yahoo.com>
2018-01-28 18:40:08 +0100

    Finito

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,27 @@
-var translation = function(words) {
-  var vowels = ['a', 'e', 'i', 'o', 'u'];
+function translate(array) {
   var result = [];
-
-  words.forEach(function(word) {
-    vowels.forEach(function(vowel) {
-      if (word.indexOf(vowel) === 0) {
-        word += "way";
+     for (var i = 0; i < array.length; i++) {
+       var n = array[i].search(/[aeiuo]/);
+       switch (n){
+         case 0: result.push(array[i]+"way"); break;
+         default :
+           result.push(array[i].substr(n)+array[i].substr(0,1).toUpperCase()+array[i].substring(1,n)+"ay");
+           //result[i]=array[i].replace(/([^aeiou]*)([aeiou])(\w+)/, "$2$3$1ay");
+         break;
       }
-    });
-    result.push(word);
-  });
+     }
 
-  return result.join(" ");
-};
+    return result.join(" ");
+
+}
+
 
 $(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
     var words = $("#sentance").val().toLowerCase().split(" ");
-    var result = translation(words);
+    var result = translate(words);
     $(".translation").text(result);
   });
 });
