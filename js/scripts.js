e5c0ce59b4488b6054fdbbe8a19708dc02747b81
Slavcho Mitrov <slavemitrov@yahoo.com>
2018-01-28 18:02:35 +0100

    Add logic to handle single letter words begining with a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
 var pigLatinTranslator = function(word) {
+  var vowels = ["a","e","i","o","u"];
+  for (var i = 0; i < vowels.length; i++) {
+    if (word.includes(vowels[i])) {
+      word += "ay";
+    }
+  }
   return word;
 };
 
