45ba3244f8f6d1886d3485a015c117b009702fdd
Slavcho Mitrov <slavemitrov@yahoo.com>
2018-01-30 12:52:33 +0100

    Refafactor translate function to use map method and if/else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,17 @@
-function translate(array) {
-  var result = [];
-     for (var i = 0; i < array.length; i++) {
-       var n = array[i].search(/[aeiuo]/);
-       switch (n){
-         case 0: result.push(array[i]+"way"); break;
-         default :
-           result.push(array[i].substr(n)+array[i].substr(0,1).toUpperCase()+array[i].substring(1,n)+"ay");
-           //result[i]=array[i].replace(/([^aeiou]*)([aeiou])(\w+)/, "$2$3$1ay");
-         break;
-      }
-     }
-
-    return result.join(" ");
-
-}
-
+function translate(words) {
+  var result = words.map(function(word) {
+    var n = word.search(/[aeiuo]/);
+    if (n === 0) {
+     return word+"way";
+    } else if (n === -1) {
+     return word;
+    } else {
+     return word.substr(n)+word.substr(0,1).toUpperCase()+word.substring(1,n)+"ay";
+     //return word.replace(/([^aeiou]*)([aeiou])(\w+)/, "$2$3$1ay");
+    }
+  });
+  return result.join(" ");
+};
 
 $(function() {
   $("form").submit(function(event) {
